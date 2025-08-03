62ebf88cf269c898bfc05f68029bee350cb2f848
manikandan6382 <manikandan6382.dev@gmail.com>
2025-08-04 00:41:42 +0530

    add base in vite config - mani

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
+const isGitHub = process.env.DEPLOY_TARGET === 'github';
 export default defineConfig({
   server: {
     host: true
   },
   plugins: [tailwindcss(), react()],
+  base: isGitHub ? '/jira/' : '/',
 })
